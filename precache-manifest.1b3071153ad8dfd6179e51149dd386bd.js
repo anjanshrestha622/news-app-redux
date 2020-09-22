3546e2d50563cf250218b6828f79db598e72f2ae
Anjan Shrestha <anjanshrestha40@gmail.com>
2020-09-22 22:10:07 +0545

    Updates

---
+++ precache-manifest.1b3071153ad8dfd6179e51149dd386bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "592426cbaa823a1b39a38f3a4f83c977",
+    "url": "/news-app-redux/index.html"
+  },
+  {
+    "revision": "35852c4e47f954b704c2",
+    "url": "/news-app-redux/static/css/main.e34bcd0b.chunk.css"
+  },
+  {
+    "revision": "e5b0d45db7f72c6f6795",
+    "url": "/news-app-redux/static/js/2.43fc4fb3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/news-app-redux/static/js/2.43fc4fb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35852c4e47f954b704c2",
+    "url": "/news-app-redux/static/js/main.8bb455e5.chunk.js"
+  },
+  {
+    "revision": "8c38aa632c609f5d5247",
+    "url": "/news-app-redux/static/js/runtime-main.0178ba2f.js"
+  }
+]);
