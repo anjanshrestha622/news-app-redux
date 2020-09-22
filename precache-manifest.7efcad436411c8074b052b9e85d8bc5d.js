1d6918dbe1a76ff96670f3e36e0f481e1301e310
Anjan Shrestha <anjanshrestha40@gmail.com>
2020-09-22 22:11:02 +0545

    Updates

--- precache-manifest.7efcad436411c8074b052b9e85d8bc5d.js
+++ precache-manifest.7efcad436411c8074b052b9e85d8bc5d.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "592426cbaa823a1b39a38f3a4f83c977",
+    "revision": "1b30cf207fcb0c3742964edebe739f12",
     "url": "/news-app-redux/index.html"
   },
   {
-    "revision": "35852c4e47f954b704c2",
+    "revision": "358011f7491d7951cb7c",
     "url": "/news-app-redux/static/css/main.e34bcd0b.chunk.css"
   },
   {
-    "revision": "e5b0d45db7f72c6f6795",
-    "url": "/news-app-redux/static/js/2.43fc4fb3.chunk.js"
+    "revision": "ecd05d4cb5b40fcb0c1c",
+    "url": "/news-app-redux/static/js/2.781e4107.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/news-app-redux/static/js/2.43fc4fb3.chunk.js.LICENSE.txt"
+    "url": "/news-app-redux/static/js/2.781e4107.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "35852c4e47f954b704c2",
-    "url": "/news-app-redux/static/js/main.8bb455e5.chunk.js"
+    "revision": "358011f7491d7951cb7c",
+    "url": "/news-app-redux/static/js/main.e0efb12b.chunk.js"
   },
   {
     "revision": "8c38aa632c609f5d5247",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/news-app-redux/precache-manifest.1b3071153ad8dfd6179e51149dd386bd.js"
+  "/news-app-redux/precache-manifest.7efcad436411c8074b052b9e85d8bc5d.js"
 );
 
 self.addEventListener('message', (event) => {
